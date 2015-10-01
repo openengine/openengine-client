bf4665f4567f7c56ba8d5a36489d1772964ecce9
Dave Bryand <dave@davebryand.com>
2015-10-01 07:48:38 -0700

    Init

---
+++ src/app/components/main.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var Bootstrap = require('react-bootstrap');
+
+var Navigation = require('./navigation.jsx');
+
+var Main = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+
+                <Navigation projectName="react-bootstrap-starter" />
+
+                <div className="container">
+                    <div className="starter-template">
+                        <h1>Bootstrap starter template</h1>
+                        <p className="lead">Use this document as a way to quickly start any new project.
+                            <br />
+                        All you get is this text and a mostly barebones HTML document.</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+});
+
+module.exports = Main;
