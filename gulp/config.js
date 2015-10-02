08719c22f7cf0f4f55de930e2faa3dfefbfc3916
Dave Bryand <dave@davebryand.com>
2015-10-01 20:36:27 -0700

    Remove less

--- gulp/config.js
+++ gulp/config.js
@@ -1,35 +1,28 @@
-var dest = './build',
-    src = './src';
+var dest = './build';
+var src = './src';
 
 module.exports = {
-    browserSync: {
-        server: {
-            baseDir: [dest, src]
-        },
-        files: [
-            dest + '/**'
-        ]
+  browserSync: {
+    server: {
+      baseDir: [dest, src]
     },
-    less: {
-        src: src + '/less/main.less',
-        watch: [
-            src + '/less/**'
-        ],
-        dest: dest
-    },
-    markup: {
-        src: src + "/www/**",
-        dest: dest
-    },
-    browserify: {
-        // Enable source maps
-        debug: true,
-        // A separate bundle will be generated for each
-        // bundle config in the list below
-        bundleConfigs: [{
-            entries: src + '/app/app.jsx',
-            dest: dest,
-            outputName: 'app.js'
-        }]
-    }
+    files: [
+      dest + '/**'
+    ]
+  },
+  markup: {
+    src: src + "/www/**",
+    dest: dest
+  },
+  browserify: {
+    // Enable source maps
+    debug: true,
+    // A separate bundle will be generated for each
+    // bundle config in the list below
+    bundleConfigs: [{
+      entries: src + '/app/app.jsx',
+      dest: dest,
+      outputName: 'app.js'
+    }]
+  }
 };
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,4 +1,3 @@
-
 /* Notes:
    - gulp/tasks/browserify.js handles js recompiling with watchify
    - gulp/tasks/browserSync.js watches and reloads compiled files
@@ -8,6 +7,5 @@ var gulp   = require('gulp');
 var config = require('../config');
 
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
-  gulp.watch(config.less.watch, ['less']);
   gulp.watch(config.markup.src, ['markup']);
 });
